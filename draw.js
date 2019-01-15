7bcaf1634b040121a3fbfb43f47688becd8b925d
Alex Rueda U <alexruedau@gmail.com>
2019-01-15 17:25:54 +0100

    Making function drawByClick works just moving the code

--- draw.js
+++ draw.js
@@ -1,33 +1,10 @@
-var text = document.getElementById(nLines);
-var myBoton = document.getElementById(boton);
+var text = document.getElementById("nLines");
+var myBoton = document.getElementById("boton");
 boton.addEventListener("click", drawByClick);
 
 var draw = document.getElementById("my_draw");
+var myCanvasWidth = draw.width;
 var myCanvas = draw.getContext("2d");
-var lines = 30;
-var l = 0;
-var yi, xf, xf_2;
-var myColor = "green";
-
-//while (l < lines) {
-//  yi = 10 * l;
-//  xf = 10 * (l + 1);
-//  drawingLine(myColor, 0, yi, xf, 300);
-//  console.log("Line " + l)
-//  l++;
-//}
-
-for (l = 0; l < lines; l++) {
-  yi = 10 * l;
-  xf = 10 * (l + 1);
-  xf_2 = 300 - (l * 10);
-  drawingLine(myColor, 0, yi, xf, 300);
-  drawingLine(myColor, 300, yi, xf_2, 300);
-  console.log("Line " + l);
-}
-
-drawingLine(myColor, 1, 1, 1, 300);
-drawingLine(myColor, 1, 299, 299, 299);
 
 function drawingLine(color, xinitial, yinitial, xfinal, yfinal) {
   myCanvas.beginPath();
@@ -39,5 +16,30 @@ function drawingLine(color, xinitial, yinitial, xfinal, yfinal) {
 }
 
 function drawByClick() {
-  alert("Don't touch me there!")
+
+  var lines = parseInt(text.value);
+  var l = 0;
+  var yi, xf, xf_2;
+  var myColor = "green";
+  var space = myCanvasWidth / lines;
+
+  //while (l < lines) {
+  //  yi = 10 * l;
+  //  xf = 10 * (l + 1);
+  //  drawingLine(myColor, 0, yi, xf, 300);
+  //  console.log("Line " + l)
+  //  l++;
+  //}
+
+  for (l = 0; l < lines; l++) {
+    yi = space * l;
+    xf = space * (l + 1);
+    xf_2 = 300 - (l * space);
+    drawingLine(myColor, 0, yi, xf, 300);
+    drawingLine(myColor, 300, yi, xf_2, 300);
+    console.log("Line " + l);
+  }
+
+  drawingLine(myColor, 1, 1, 1, 300);
+  drawingLine(myColor, 1, 299, 299, 299);
 }
